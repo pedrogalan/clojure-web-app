b809035b9e041807b75aa5fa19999d2d195588e2
Pedro Galan <pedro.galan@piksel.com>
2020-04-11 15:52:33 +0100

    Connect with database and fetch content

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring "1.7.1"]
                  [compojure "1.6.1"]
-                 [ring/ring-json "0.5.0"]]
+                 [ring/ring-json "0.5.0"]
+                 [org.clojure/java.jdbc "0.7.11"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]]
   :repl-options {:init-ns clojure-web-app.core}
   :main clojure-web-app.core)
--- src/clojure_web_app/core.clj
+++ src/clojure_web_app/core.clj
@@ -4,11 +4,22 @@
             [compojure.core       :refer [routes GET POST]]
             [compojure.route      :refer [not-found]]
             [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
-            [ring.util.response   :refer [response]]))
+            [ring.util.response   :refer [response]]
+            [clojure.java.jdbc    :as    sql]))
+
+(def db-connection {:classname "org.postgresql.Driver"
+                    :subprotocol "postgresql"
+                    :subname "//localhost:5432/books"
+                    :user "postgres"
+                    :password "postgres"})
+
+(defn get-all-customers []
+  (sql/query db-connection ["select * from books"]))
 
 (def my-routes-handler
   (routes
     (GET  "/info"  []      (response {:version "0.1" :created "April 11th 2020"}))
+    (GET  "/books" []      (response (get-all-customers)))
     (POST "/debug" request (response (with-out-str (pprint request))))
     (not-found "<h1>Page not found</h1>")))
 
