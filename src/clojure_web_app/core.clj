13813c7fe855623ce7e038dc7e0ceb4a94bb8222
Pedro Galan <pedro.galan@piksel.com>
2020-04-11 11:30:10 +0100

    Add ring middleware to handle request/response with JSON wrappers

--- src/clojure_web_app/core.clj
+++ src/clojure_web_app/core.clj
@@ -1,13 +1,19 @@
 (ns clojure-web-app.core
-  (:require [ring.adapter.jetty :refer [run-jetty]]
-            [clojure.pprint     :refer [pprint]]
-            [compojure.core     :refer [routes GET]]
-            [compojure.route    :refer [not-found]]))
+  (:require [ring.adapter.jetty   :refer [run-jetty]]
+            [clojure.pprint       :refer [pprint]]
+            [compojure.core       :refer [routes GET POST]]
+            [compojure.route      :refer [not-found]]
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
+            [ring.util.response   :refer [response]]))
 
 (def my-routes-handler
   (routes
-    (GET  "/info"  [] "<h1>Information</h1> This is information about the application")
+    (GET  "/info"  []      (response {:version "0.1" :created "April 11th 2020"}))
+    (POST "/debug" request (response (with-out-str (pprint request))))
     (not-found "<h1>Page not found</h1>")))
 
+(def app
+  (wrap-json-body (wrap-json-response my-routes-handler)))
+
 (defn -main []
-  (run-jetty my-routes-handler {:port 3000}))
+  (run-jetty app {:port 3000}))
